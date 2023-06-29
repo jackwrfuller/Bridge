a987c538951cdec4f7e1196852e6716d23c16a80
Jack Fuller <u6100349@anu.edu.au>
2023-06-30 08:26:36 +1000

    Allow Cards to be constructed using a card string

--- src/main/java/jwrf/CardStringEncoder.java
+++ src/main/java/jwrf/CardStringEncoder.java
@@ -2,12 +2,40 @@ package jwrf;
 
 public class CardStringEncoder {
 
+    /**
+     * Returns a Value from a correct card string. E.g Given "AS" it returns Value.ACE
+     *
+     */
     public static Value getValueFromString(String cardString) {
-        return Value.ACE;
+        assert (isValidCardString(cardString)) : "Invalid card string: " + cardString;
+        // Case where value is "10", the only card string with three characters
+        if (cardString.length() == 3) {
+            return Value.get("10");
+        }
+        String[] strArr = cardString.split("");
+        return Value.get(strArr[0]);
     }
 
     public static Suit getSuitFromString(String cardString) {
-        return Suit.SPADES;
+        assert (isValidCardString(cardString)) : "Invalid card string: " + cardString;
+        String suitString = cardString.length() == 3 ? String.valueOf(cardString.charAt(2)) : String.valueOf(cardString.charAt(1));
+        return Suit.get(suitString);
+    }
+
+    private static boolean isValidCardString(String cardString) {
+        if (cardString.length() > 3 || cardString.length() < 2) {
+            return false;
+        }
+        if (cardString.length() == 3) {
+            String[] strArr = cardString.split("");
+            if (!strArr[0].equals("1") || !strArr[1].equals("0") || !strArr[2].matches("[SDHC]") ) {
+                return false;
+            }
+        }
+        if (cardString.length() == 2 && !cardString.matches("[AKQJ98765432][SDHC]")) {
+            return false;
+        }
+        return true;
     }
 
 }
