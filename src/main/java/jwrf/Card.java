7e1fa1cc514bdca7efc64687804502e9f7af5b37
jackwrfuller <78133717+jackwrfuller@users.noreply.github.com>
2023-06-29 21:06:37 +1000

    Create CardStringEncoder

--- src/main/java/jwrf/Card.java
+++ src/main/java/jwrf/Card.java
@@ -1,6 +1,11 @@
 package jwrf;
 
-public record Card(Suit suit, Value value) {
+public record Card(Value value, Suit suit) {
+
+    public Card(String cardString) {
+        this(CardStringEncoder.getValueFromString(cardString), CardStringEncoder.getSuitFromString(cardString));
+    }
+
     @Override
     public String toString() {
         return value.toString() + suit.toString();
---
+++ src/main/java/jwrf/CardStringEncoder.java
@@ -0,0 +1,13 @@
+package jwrf;
+
+public class CardStringEncoder {
+
+    public static Value getValueFromString(String cardString) {
+        return Value.ACE;
+    }
+
+    public static Suit getSuitFromString(String cardString) {
+        return Suit.SPADES;
+    }
+
+}
